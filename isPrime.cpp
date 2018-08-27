74e98fe419514e6e07b68b0ed9333ef2a3d46f64
lishilin <1273043991@qq.com>
2018-08-27 20:35:37 +0800

    add isPrime file

---
+++ isPrime.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+
+bool isPrime(int data);
+
+int main()
+{
+    int data(0);
+    bool isPrim(false);
+
+    do {
+        printf("input a number bigger than two: ");
+        scanf("%d", &data);
+    }while(data <= 2);
+    
+    isPrim=isPrime(data);
+    
+    if(isPrim) {
+        printf("%d is a prime\n", data);
+    }
+    else {
+         printf("%d is not a prime\n", data);
+    }
+}
+
+bool isPrime(int data)
+{
+    int sdata = int(sqrt(data));
+    for(int i = 2; i <= sdata; i++){
+
+    if(!(data % i)){
+        return false;
+    }
+
+    }
+    return true;
+}
